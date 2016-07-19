67b600dc95cc805181489e889b93e03985975941
meolunr <meolunr@gmail.com>
2016-07-19 16:58:31 +0800

    Modify releasetools.py

--- releasetools.py
+++ releasetools.py
@@ -1,6 +1,43 @@
 import common
 import edify_generator
 
+def ModifyBegin(info):
+  edify = info.script
+  edify.script[0] = \
+  '''ifelse(is_mounted("/system"), unmount("/system"));
+ui_print("******************************************");
+ui_print("* Flyme for Nubia NX503A");
+ui_print("*");
+ui_print("* Romer: Leon");
+ui_print("* http://weibo.com/SkyTrous");
+ui_print("******************************************");\n''' + edify.script[0]
+
+  return
+
+def ModifyCommand(info):
+  edify = info.script
+
+  for i in xrange(len(edify.script)):
+    if "mount(" in edify.script[i] and "by-name/system" in edify.script[i]:
+      edify.script[i] = 'mount("ext4", "EMMC", "/dev/block/platform/msm_sdcc.1/by-name/system", "/system");'
+    elif "package_extract_dir(" in edify.script[i] and "recovery" in edify.script[i]:
+      edify.script[i] = 'ui_print("{*} Extracting System");'
+
+      return
+
+def AddPrompt(info):
+  edify = info.script
+
+  for i in xrange(len(edify.script)):
+    if "format(" in edify.script[i] and "by-name/system" in edify.script[i]:
+      edify.script[i] = 'ui_print("{*} Formatting System");\n' + edify.script[i]
+    elif 'set_metadata_recursive("/system"' in edify.script[i]:
+      edify.script[i] = 'ui_print("{*} Setting Permissions");\n' + edify.script[i]
+    elif 'package_extract_file("boot' in edify.script[i]:
+      edify.script[i] = 'ui_print("{*} Flashing Kernel");\n' + edify.script[i]
+
+      return
+
 def AddSuperSU(info):
     SuperSU = info.input_zip.read("META/SuperSU.zip")
     common.ZipWriteStr(info.output_zip, "SuperSU/SuperSU.zip", SuperSU)
@@ -12,4 +49,7 @@ def AddSuperSU(info):
     info.script.AppendExtra(('ui_print("{*} Finish!");'))
 
 def FullOTA_InstallEnd(info):
+    ModifyBegin(info)
+    ModifyCommand(info)
+    AddPrompt(info)
     AddSuperSU(info)
